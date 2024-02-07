c7759dc4a265dde7b0eebe460090414db2baaac0
Edvard Skei Toenset <edvardskt@gmail.com>
2024-02-07 21:20:33 +0100

    more on fsm

--- ProjectFolder/fsm/fsm.go
+++ ProjectFolder/fsm/fsm.go
@@ -3,12 +3,21 @@ package fsm
 import (
 	"Sanntid/elevator"
 	"Sanntid/elevator_io"
+	"Sanntid/elevio"
 )
 
 var elev elevator.Elevator
 var outputDevice elevator_io.ElevOutputDevice
 
 func init() {
-	elev = 
+	elev = elevator.Elevator_uninitialized()
+	outputDevice = elevator_io.Elevio_getOutputDevice()
 }
 
+func setAllLights(es elevator.Elevator) {
+	for floor := 0; floor < elevator_io.N_FLOORS; floor++ {
+		for btn := 0; btn < elevator_io.N_BUTTONS; btn++ {
+			outputDevice.RequestButtonLight(floor, elevio.ButtonType(btn), es.requests[floor][btn])
+		}
+	}
+}
