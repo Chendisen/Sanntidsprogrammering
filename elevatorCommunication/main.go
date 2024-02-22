9459a63dd549c4c51c26a2285cd92ec75b2b6edd
Edvard Skei Toenset <edvardskt@gmail.com>
2024-02-22 15:37:29 +0100

    Implemented test function for communication

--- elevatorCommunication/main.go
+++ elevatorCommunication/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"Communication/broadcast"
+	"Communication/recieve"
 )
 
 func main() {
-	broadcast.Peer_broadcastAlive()
+	go broadcast.Peer_broadcastAlive()
+	go recieve.Peer_recieveAlive()
 }
