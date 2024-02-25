0492fcae4679ad07f94a4caf8d804b6589ae1e9f
Edvard Skei Toenset <edvardskt@gmail.com>
2024-02-25 17:55:14 +0100

    CyclicCounter

---
+++ ProjectFolder/cyclicCounter/cyclicCounter.go
@@ -0,0 +1,45 @@
+package cyclicCounter
+
+type Counter struct {
+	Value int
+	Max int
+	ShouldReset bool
+}
+
+func MakeCounter(max int) Counter{
+	return Counter{Value: 0, Max: max, ShouldReset: false}
+}
+
+func Reset(counter *Counter){
+	counter.Value = 0
+	counter.ShouldReset = false
+}
+
+func ShouldUpdate(recieved Counter, current Counter) bool{
+	var shouldUpdate bool = false
+
+	if(recieved.Value > current.Value) {
+		shouldUpdate = true
+	} else if(recieved.Value < current.Value && current.ShouldReset){
+		shouldUpdate = true
+	} 
+
+	return shouldUpdate
+}
+
+func Increment(counter *Counter){
+	if(counter.Value == counter.Max){
+		Reset(counter)
+	} else {
+		counter.Value++
+	}
+}
+
+func UpdateValue(counter *Counter, value int){
+	if(counter.Value > value){
+		counter.Value = value
+		counter.ShouldReset = false
+	} else {
+		counter.Value = value
+	}
+}
